b814fe32280cc179f2df1dd1270a25f529cd8c24
Качмазов Максим <maxim.kachmazov@yandex.ru>
2025-09-14 17:06:33 +0300

    7.7 Наращивание тестовой базы и рефакторинг

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -1,5 +1,5 @@
+import re
 from playwright.sync_api import Page
-
 from components.authentication.registration_form_component import RegistrationFormComponent
 from elements.button import Button
 from elements.link import Link
@@ -17,6 +17,7 @@ class RegistrationPage(BasePage):
 
     def click_login_link(self):
         self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login"))
 
     def click_registration_button(self):
         self.registration_button.click()
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
-from playwright.sync_api import Page
+from typing import Pattern
+from playwright.sync_api import Page, expect
 
 
 class BasePage:
@@ -9,4 +10,7 @@ class BasePage:
         self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
-        self.page.reload(wait_until='networkidle')
+        self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: Pattern[str]):
+        expect(self.page).to_have_url(expected_url)
